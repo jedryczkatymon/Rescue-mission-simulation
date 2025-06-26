77b147f3d104f949e00b4530ac8eb7d5cf01194c
Tymon Jędryczka <jedryczkatymon@gmail.com>
2025-06-26 14:40:05 +0200

    Stop tracking build directories

--- build/CMakeFiles/RESCUE_MISSION_SIMULATION.dir/src/Hostage.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/RESCUE_MISSION_SIMULATION.dir/src/Hostage.cpp.o: \
- /home/jedryczkatymon/Rescue-mission-simulation/src/Hostage.cpp \
- /usr/include/stdc-predef.h \
- /home/jedryczkatymon/Rescue-mission-simulation/include/Hostage.h
--- build/CMakeFiles/RESCUE_MISSION_TESTS.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for RESCUE_MISSION_TESTS.
-# This may be replaced when dependencies are built.
